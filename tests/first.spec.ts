d1d5401195deef6bf1ab4bb3dd31f389507ce55b
HiroyukiIsoe <hiroyuki.isoe@toranoana.co.jp>
2022-03-14 22:24:29 +0900

    記事書くうちの変更を反映

--- tests/first.spec.ts
+++ tests/first.spec.ts
@@ -2,6 +2,6 @@ import { test, expect } from '@playwright/test';
 
 test('basic test', async ({ page }) => {
   await page.goto('https://demo.playwright.dev/todomvc/');
-  const title = page.locator('.header h1');
+  const title = page.locator('header h1');
   await expect(title).toHaveText('todos');
 });
